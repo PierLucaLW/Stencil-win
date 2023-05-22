3320543414e29d307954afeb293afe6cc0c573b4
PierLucaLW <48440773+PierLucaLW@users.noreply.github.com>
2023-05-22 10:43:51 +0200

    test win os condition

--- Sources/Stencil/Variable.swift
+++ Sources/Stencil/Variable.swift
@@ -111,7 +111,7 @@ public struct Variable: Equatable, Resolvable {
     } else if let string = context as? String {
       return resolve(bit: bit, collection: string)
     } else if let object = context as? NSObject {  // NSKeyValueCoding
-      #if os(Linux || Windows)
+      #if os(Windows)
         return nil
       #else
         if object.responds(to: Selector(bit)) {
